d22c9e4fc6d6c6a8a9262a7018b26efc56b50e61
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-01-09 16:08:18 -0300

    Initial commit

---
+++ 2_FUNDAMENTOS/1_modulo_interno/index.js
@@ -0,0 +1,8 @@
+const meuModulo = require('./meu_modulo') //importando o modulo que criei
+const soma = meuModulo.soma
+const multiplica = meuModulo.multiplica
+
+soma(2 , 3)
+soma(5 , 10)
+multiplica(5 , 5)
+multiplica(3 , 10)
---
+++ 2_FUNDAMENTOS/1_modulo_interno/meu_modulo.js
@@ -0,0 +1,8 @@
+module.exports ={
+    soma(a , b){ //Quando estou exportando não precisa do function
+        console.log(a + b)
+    },
+    multiplica(a , b){
+        console.log(a * b)
+    }
+}
