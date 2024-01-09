5dd528d3a5606f660e153f12c0c2d8e21fef430a
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-01-09 18:38:49 -0300

    Atualizando

---
+++ 2_FUNDAMENTOS/3_core_module/index.js
@@ -0,0 +1,5 @@
+const path = require('path')
+
+const extension = path.extname("arquivo.pdf") //extrai a extensão do arquivo
+
+console.log(extension)
---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,20 @@
+const x = 10
+const y = 'Algum texto'
+const z = [1 , 2]
+
+console.log(x , y , z)
+
+//contagem de impressões
+console.count('Teste')
+console.count('Teste')
+console.count('Teste')
+console.count('Teste')
+console.count('Teste')
+
+//variavel entre string
+console.log("%s" , y)
+
+//limpar o console
+setTimeout(()=>{
+    console.clear()
+},2000)
